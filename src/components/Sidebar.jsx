a02c3f4a31d7e48e2979850674fd6f838fc29ea6
Rallefar1103 <rasmus@turftank.com>
2024-01-19 22:32:24 -0800

    update files

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
-import { RiCloseLine } from "react-icons/ri";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { HiOutlineHashtag, HiOutlineHome, HiOutlineMenu, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
+import { RiCloseLine } from 'react-icons/ri';
 
-import { logo } from "../assets";
-import { links } from "../assets/constants.js";
+import { logo } from '../assets';
+
+const links = [
+  { name: 'Discover', to: '/', icon: HiOutlineHome },
+  { name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
+  { name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
+  { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
+];
 
 const NavLinks = ({ handleClick }) => (
   <div className="mt-10">
@@ -29,6 +36,20 @@ const Sidebar = () => {
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
+      </div>
+
+      {/* Mobile sidebar */}
+      <div className="absolute md:hidden block top-6 right-3">
+        {!mobileMenuOpen ? (
+          <HiOutlineMenu className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(true)} />
+        ) : (
+          <RiCloseLine className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(false)} />
+        )}
+      </div>
+
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
   );
