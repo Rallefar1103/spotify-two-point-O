fd0bf2a3d72185c1e46b3ddb3a93a47e3bea136d
Rallefar1103 <rasmus@turftank.com>
2024-01-19 19:52:40 -0800

    adds more components

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,8 @@
-const PlayPause = () => (
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay}) => (
   <div>Loader</div>
 );
 
 export default PlayPause;
+ 
